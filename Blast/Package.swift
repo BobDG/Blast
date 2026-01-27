db1eafadb677f3a2d38966a841700e96d5cc7620
Bob de Graaf <graafict@gmail.com>
2026-01-27 17:04:00 +0100

    - Deleted podspec and added Package swift

---
+++ Blast/Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "Blast",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "Blast", targets: ["Blast"])
+    ],
+    targets: [
+        .target(
+            name: "Blast",
+            path: "."
+        )
+    ]
+)
